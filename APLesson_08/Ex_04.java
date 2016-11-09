d31b3a3d7ca07811f2ad0dd6e68626ca3f71a61f
willnute <willnute@gmail.com>
2016-11-09 10:56:23 -0800

    Lesson 08
    Finished 08.4

--- APLesson_08/Ex_04.java
+++ APLesson_08/Ex_04.java
@@ -15,10 +15,13 @@ public class Ex_04
 	
 	public static String tree(String word, int start, int stop)
 	{
-		if(start <= stop){
-			System.out.println(word.substring(0,start));
+		if(start <= stop)
+		{
+			System.out.printf("%10s%n",word.substring(0,start));
 			start += 1;
-			tree(word, start, stop);
+			return tree(word, start, stop);
 		}
+		else 
+			return "";
 	}
 }
